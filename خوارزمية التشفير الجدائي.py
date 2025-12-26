2253929140784dd3da1cba143c6f3343691761d2
megatron77777 <khadourmohamad2@gmail.com>
2025-12-26 12:01:21 +0300

    Create خوارزمية التشفير الجدائي.py

---
+++ خوارزمية التشفير الجدائي.py
@@ -0,0 +1,14 @@
+def multiplicative_encryption(plainText, key):
+    cipherText = ""
+    n = 26
+    
+    for char in plainText:
+        if 'A' <= char <= 'Z':
+            idx = ord(char) - ord('A')
+            cipher_idx = (idx * key) % n
+            cipherChar = chr(cipher_idx + ord('A'))
+            cipherText += cipherChar
+        else:
+            cipherText += char
+            
+    return cipherText
